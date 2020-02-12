f7f4d3d530de638bd7e26607cfe1d609da49bb52
Nebula <adenosinebula@airmail.cc>
2020-02-12 00:10:45 +0000

    Wrap app in a rectangle to simulate our LCD screen

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Segment } from 'semantic-ui-react';
+import './style.css';
 
 export function App() {
 	return (
-		<div>
+		<div className='lcd-wrapper'>
 			<Segment>
 				Hello world
 			</Segment>
