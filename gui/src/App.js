256abb6427f8e32bc0b845bea2104b28a56d99fc
Nebula <adenosinebula@airmail.cc>
2020-02-13 23:18:48 +0000

    Create basic header

--- gui/src/App.js
+++ gui/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
-import { Segment } from 'semantic-ui-react';
+import { Icon, Menu, Segment } from 'semantic-ui-react';
 import './style.css';
 
 export function App() {
 	return (
 		<div className='lcd-wrapper'>
-			<Segment>
-				Hello world
-			</Segment>
+			<Menu>
+				<Menu.Item
+					icon
+					onClick={console.log}
+				>
+					<Icon name='chevron left' />
+				</Menu.Item>
+
+				<Menu.Item header>
+					Home
+				</Menu.Item>
+
+				<Menu.Item
+					icon
+					position='right'
+					as={Link}
+					to='/'
+				>
+					<Icon name='home' />
+				</Menu.Item>
+			</Menu>
 		</div>
 	);
 }
