7e51b1e2c4d406595785cf1414cb690fa39b4d6f
Nebula <adenosinebula@airmail.cc>
2020-02-27 00:57:39 +0000

    Add API route to test relays

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,19 @@
 from flask import Flask
 app = Flask(__name__)
 
+import relay
+import relay_test
+
 @app.route('/')
 def index():
     return '<h1>Hello world!</h1>'
 
+@app.route('/test/relays')
+def test_relays():
+    relay_test.test_relays()
+    return ''
+
 if __name__ == '__main__':
+    relay.init_relays()
+
     app.run(host='0.0.0.0', use_reloader=True)
