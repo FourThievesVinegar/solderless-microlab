e55567e49f574b0f90d7bda71a790a7ea94b4dbd
Nebula <adenosinebula@airmail.cc>
2020-02-27 01:23:29 +0000

    Hook relay test button up to backend

--- gui/src/Tests.js
+++ gui/src/Tests.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Button, Icon, Menu, Segment } from 'semantic-ui-react';
 import './style.css';
 import { Header } from './Header.js';
+import { apiUrl } from './utils.js';
+
+export function TestButton(props) {
+	const { name, label } = props;
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(false);
+
+	const runTest = (e) => {
+		if (loading) return;
+		setLoading(true);
+
+		fetch(apiUrl + 'test/' + name)
+		.then(res => {
+			setLoading(false);
+			setError(false);
+		})
+		.catch(err => {
+			setLoading(false);
+			setError(true);
+		});
+	};
+
+	return (
+		<Button onClick={runTest} loading={loading}>
+			{error ? 'Error' : label}
+		</Button>
+	);
+};
 
 export function Tests() {
 	return (
@@ -10,10 +38,8 @@ export function Tests() {
 			<Header>Hardware Tests</Header>
 
 			<div className='button-menu'>
-				<Button onClick={console.log}>
-					Test Relays
-				</Button>
+				<TestButton name='relays' label='Test Relays' />
 			</div>
 		</div>
 	);
-}
+};
---
+++ gui/src/utils.js
@@ -0,0 +1,1 @@
+export const apiUrl = 'http://192.168.0.24:5000/';
