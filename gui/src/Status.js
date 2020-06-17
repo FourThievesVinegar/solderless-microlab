5d7a2564002ec06ace538b89167318753400b8af
Nebula <adenosinebula@airmail.cc>
2020-06-17 23:28:45 +0000

    Display loading indicator on option buttons

--- gui/src/Status.js
+++ gui/src/Status.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Button, Icon, Menu, Segment } from 'semantic-ui-react';
 import './style.css';
@@ -7,12 +7,20 @@ import { apiUrl } from './utils.js';
 
 export function Status(props) {
 	const { status } = props;
+	const [loading, setLoading] = useState(false);
 
 	const selectOption = (option) => {
+		if (loading) return;
+		setLoading(true);
+
 		fetch(apiUrl + 'select/option/' + option)
 			.then(response => response.json())
 			.then(data => console.log(data));
 	};
+
+	useEffect(() => {
+		setLoading(false);
+	}, [status && status.step]);
 
 	return (
 		<div>
@@ -23,7 +31,7 @@ export function Status(props) {
 					<p>{status.message}</p>
 
 					{status.options.map(x =>
-						<Button key={x} onClick={() => selectOption(x)}>
+						<Button key={x} onClick={() => selectOption(x)} loading={loading}>
 							{x}
 						</Button>
 					)}
