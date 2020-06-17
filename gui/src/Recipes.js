b2c178dc05fa2504e93990df965f9b098a6c1b56
Nebula <adenosinebula@airmail.cc>
2020-06-17 22:46:41 +0000

    Load the list of recipies from the API

--- gui/src/Recipes.js
+++ gui/src/Recipes.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Button, Icon, Menu, Segment } from 'semantic-ui-react';
 import './style.css';
 import { Header } from './Header.js';
+import { apiUrl } from './utils.js';
 
 export function Recipes() {
+	const [recipies, setRecipies] = useState(false);
+
+	useEffect(() => {
+		fetch(apiUrl + 'list')
+			.then(response => response.json())
+			.then(data => setRecipies(data));
+	}, []);
+
+	console.log(recipies);
+
 	return (
 		<div>
 			<Header>Recipes</Header>
 
-			<p>Recipes page here...</p>
+			{recipies ?
+				<p>{recipies}</p>
+			:
+				<p>Loading...</p>
+			}
 		</div>
 	);
 }
