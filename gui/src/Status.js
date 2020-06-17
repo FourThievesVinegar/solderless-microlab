553e251564df544ec1f5ab5f6a632730cc54a82a
Nebula <adenosinebula@airmail.cc>
2020-06-17 23:18:02 +0000

    Send status option to API on button press

--- gui/src/Status.js
+++ gui/src/Status.js
@@ -3,9 +3,16 @@ import { Switch, Route, Link } from 'react-router-dom';
 import { Button, Icon, Menu, Segment } from 'semantic-ui-react';
 import './style.css';
 import { Header } from './Header.js';
+import { apiUrl } from './utils.js';
 
 export function Status(props) {
 	const { status } = props;
+
+	const selectOption = (option) => {
+		fetch(apiUrl + 'select/option/' + option)
+			.then(response => response.json())
+			.then(data => console.log(data));
+	};
 
 	return (
 		<div>
@@ -16,7 +23,7 @@ export function Status(props) {
 					<p>{status.message}</p>
 
 					{status.options.map(x =>
-						<Button>
+						<Button onClick={() => selectOption(x)}>
 							{x}
 						</Button>
 					)}
