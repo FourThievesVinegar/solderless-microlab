545f33a0f2c7bcef4d4b1d19c0452d4c0adc69df
no one <noone@nowhere.com>
2022-05-09 00:50:10 +0000

    sass version and initial boot script

--- boot.sh
+++ boot.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-cd ./backend && virtualenv -p python3 env && source env/bin/activate && pip install -r requirements.txt && python main.py &
-cd ./gui && yarn install && yarn start
+#TODO: Update this to use env vars for users locations.
+
+
+cd /home/ubuntu/workspace/solderless-microlab/backend && \
+	virtualenv -p python3 env && source env/bin/activate && \
+	pip install -r requirements.txt && \
+	export FLASK_APP=main.py && \
+	export FLASK_ENV=development && \
+	export FLASK_DEBUG=0 && \
+	python main.py &
+cd /home/ubuntu/workspace/solderless-microlab/gui && yarn start
