98c8e1a9f7d12928bf0897cb8f1c779201e0a4c8
Mike Kissinger <mis4mike@gmail.com>
2022-05-17 22:45:41 -0400

    Add a startup script

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#TODO: Update this to use env vars for users locations.
+
+
+cd ./backend && \
+	virtualenv -p python3 env && source env/bin/activate && \
+	pip install -r requirements.txt && \
+	export FLASK_APP=main.py && \
+	export FLASK_ENV=development && \
+	export FLASK_DEBUG=0 && \
+	python main.py &
+cd ./gui && yarn start
