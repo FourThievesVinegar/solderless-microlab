96acffe6dba04df6e876a8161b0eaeda44a87412
Sparky <sparkles@sparkles.com>
2023-09-12 13:56:12 -0400

    Don't hot reload in prod

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 # Start the application on the configured port (default 8081)
 # Look in api.routes for the actual api code
-
+from os import environ
 from api import app
 import config
 
+reload = False if environ.get("FLASK_ENV") == 'production' else true
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=config.apiPort, use_reloader=True)
+    app.run(host='0.0.0.0', port=config.apiPort, use_reloader=reload)
