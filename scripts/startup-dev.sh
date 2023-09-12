9e70d0e2f05b7fda6f19731d607a2bb3df7d3325
Sparky <sparkles@sparkles.com>
2023-09-12 13:36:20 -0400

    Create dev and prod startup scripts.

---
+++ scripts/startup-dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#TODO: Update this to use env vars for users locations.
+
+cd ../backend && \
+	virtualenv -p python3 --system-site-packages env && source env/bin/activate && \
+	pip install -r requirements.txt && \
+	export FLASK_APP=main.py && \
+	export FLASK_ENV=development && \
+	export FLASK_DEBUG=0 && \
+	python main.py &
+
+cd ../backend && \
+	source env/bin/activate && \
+	celery -A recipes worker --loglevel=INFO &
+
+cd ../gui && yarn start
