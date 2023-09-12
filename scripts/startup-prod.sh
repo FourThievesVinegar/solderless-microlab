d42a0533aef9c74a73f54a07f9f848e0db15ca5c
Sparky <sparkles@sparkles.com>
2023-09-12 17:43:18 -0400

    Start serve in the background

--- scripts/startup-prod.sh
+++ scripts/startup-prod.sh
@@ -20,7 +20,7 @@ then
 fi
 
 # Start the GUI in serve mode, which is more performant than 'yarn start' dev mode
-cd ../gui && serve -s build
+cd ../gui && serve -s build &
 
 # Start a Browser going to localhost:3000
 xdg-open http://locahost:3000
