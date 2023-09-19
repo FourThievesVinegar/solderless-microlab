b7f0d11af52c33efa3f719c59317f7a2855f722b
Sparky <sparkles@sparkles.com>
2023-09-19 17:31:50 -0400

    Break out serve and browser scripts

--- scripts/startServe.sh
+++ scripts/startServe.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 # If there is no build directory, then build the GUI. 
 if [ ! -d "./gui/build" ]
 then
@@ -7,6 +7,3 @@ fi
 
 # Start the GUI in serve mode, which is more performant than 'yarn start' dev mode
 cd ./gui && serve -s build &
-
-# Start a Browser going to localhost:3000
-xdg-open http://localhost:3000
