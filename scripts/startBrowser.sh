102da4a8aa6068557c981c2fed89271397a843a2
Sparky <sparkles@sparkles.com>
2023-09-20 13:45:05 -0400

    Remove the hacky approach - kiosk mode works

--- scripts/startBrowser.sh
+++ scripts/startBrowser.sh
@@ -1,7 +1,4 @@
 # Start a Browser going to localhost:3000
 # xdg-open http://localhost:3000
 
-# Hacky suppression of "restore tabs" popover
-/usr/lib/chromium-browser/chromium-browser-v7 --no-startup-window --kiosk
-# The actual window we want
 /usr/lib/chromium-browser/chromium-browser-v7 --force-renderer-accessibility --enable-remote-extensions --enable-pinch --enable-crashpad --start-fullscreen --hide-crash-restore-bubble --kiosk --app=http://localhost:3000
