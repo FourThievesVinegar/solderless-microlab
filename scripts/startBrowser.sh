4e3d7edf3bc3023f7f0df45208b66d1e24960d99
Sparky <sparkles@sparkles.com>
2023-09-20 12:59:22 -0400

    don't show the restore bubble cause we don't shut down gently

--- scripts/startBrowser.sh
+++ scripts/startBrowser.sh
@@ -1,4 +1,4 @@
 # Start a Browser going to localhost:3000
 # xdg-open http://localhost:3000
 
-/usr/lib/chromium-browser/chromium-browser-v7 --force-renderer-accessibility --enable-remote-extensions --enable-pinch --enable-crashpad --start-fullscreen http://localhost:3000
+/usr/lib/chromium-browser/chromium-browser-v7 --force-renderer-accessibility --enable-remote-extensions --enable-pinch --enable-crashpad --start-fullscreen --hide-crash-restore-bubble http://localhost:3000
