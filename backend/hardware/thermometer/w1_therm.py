0f5de75fec7802a6eadd1199381a2456d548ab02
0phelia36 <84165411+0phelia36@users.noreply.github.com>
2023-12-26 18:38:26 -0500

    Only read new W1TempSensor temps once per second

--- backend/hardware/thermometer/w1_therm.py
+++ backend/hardware/thermometer/w1_therm.py
@@ -1,14 +1,18 @@
 from hardware.thermometer.base import TempSensor
 from w1thermsensor import W1ThermSensor, Sensor, SensorNotReadyError
+from datetime import datetime, timedelta
 
 class W1TempSensor(TempSensor):
     sensor = None
     lastTemp = 0
+    nextTempReadingTime = None
+
     def __init__(self):
         """
         Constructor. Initializes the sensor.
         """
         self.sensor = W1ThermSensor()
+        self.nextTempReadingTime = datetime.now()
 
     def getTemp(self):
         """
@@ -16,8 +20,13 @@ class W1TempSensor(TempSensor):
         :return:
             Temperature in Celsius
         """
-        try:
-            self.lastTemp = self.sensor.get_temperature()
-        except SensorNotReadyError:
-            pass
+        # With the DS18S20 at least reading new data seems to take ~0.8 seconds,
+        # so only read new temp if our temp is over 1 second old to prevent
+        # blocking threads for data that hasn't changed much
+        if datetime.now() > self.nextTempReadingTime:
+            try:
+                self.lastTemp = self.sensor.get_temperature()
+                self.nextTempReadingTime = datetime.now() + timedelta(seconds=1)
+            except SensorNotReadyError:
+                pass
         return self.lastTemp
