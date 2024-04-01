676aeb773c27601c4cc0ea1e07960f010c7a6b2d
Sparkles <sparklesthesparklyunicorn@gmail.com>
2024-04-01 15:10:39 -0400

    Install python packages as root to run as root

--- scripts/startFlask.sh
+++ scripts/startFlask.sh
@@ -3,6 +3,6 @@
 cd ./backend
 python3 -m venv env
 source env/bin/activate
-pip3 install -r requirements.txt
+sudo pip3 install -r requirements.txt
 
-sudo python main.py production
+sudo python3 main.py production
