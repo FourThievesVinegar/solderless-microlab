625b34beec2621e7f2ba11c95a1caa76f54e34b2
Sparkles <2106250+sparklestheunicorn@users.noreply.github.com>
2024-07-23 14:03:32 -0400

    Fix serial temperature reading and simplify waiting between

--- backend/hardware/thermometer/serial.py
+++ backend/hardware/thermometer/serial.py
@@ -39,62 +39,63 @@ class SerialTempSensor(TempSensor):
         :return:
             Temperature in Celsius
         """
-        if datetime.now() > self.nextTempReadingTime:
-            line = "12345678901"
-            lastLine = ""
-            while line.find("\n") == -1 or line.find("=") == -1 or line.find(".") == -1:
-                lastLine = line
-                try:
-                    line = self.tempSer.readline().decode()
-                except Exception as e:
-                    logging.error("Error reading from thermometer")
-                    logging.exception(str(e))
-                    continue
-                finally:
-                    logging.debug("ser read " + str(len(line)) + " " + line)
-                    time.sleep(0.5)
+        if datetime.now() < self.nextTempReadingTime:
+            return self.lastTemp
 
-            lastLine = str(line)
+        line = "12345678901"
+        lastLine = ""
+        while line.find("\n") == -1 or line.find("=") == -1 or line.find(".") == -1:
+            lastLine = line
+            try:
+                line = self.tempSer.readline().decode()
+            except Exception as e:
+                logging.error("Error reading from thermometer")
+                logging.exception(str(e))
+                continue
+            finally:
+                logging.debug("ser read " + str(len(line)) + " " + line)
+                time.sleep(0.5)
 
-            # Look for 't1=' or 't=' in the input line
-            # Unclear why sometimes the thermometer returns 't1=' and other times just 't='
-            # Use rfind because we want the last '=' and sometimes input includes extras
-            start = lastLine.rfind("=") + len("=")
+        lastLine = str(line)
 
-            end = lastLine.find("\\n", start)
-            if (
-                end == -1
-            ):  # Different thermometers may parse differently. These conditionals may need to expand.
-                end = lastLine.find(" ", start)
-            if end == -1:  # Maybe just go to the end?
-                end = len(lastLine) - 1
+        # Look for 't1=' or 't=' in the input line
+        # Unclear why sometimes the thermometer returns 't1=' and other times just 't='
+        # Use rfind because we want the last '=' and sometimes input includes extras
+        start = lastLine.rfind("=") + len("=")
 
-            logging.debug(
-                "found "
-                + str(start)
-                + " "
-                + str(end)
-                + " "
-                + lastLine
-                + " "
-                + lastLine[start:end]
-            )
-            # Make sure that we have a start and an end and that there is something between them
-            if start > -1 and end > -1 and end - start > 2:
-                try:
-                    self.lastTemp = float(lastLine[start:end])
-                except Exception as e:
-                    logging.error("Error converting temperature reading")
-                    logging.error(lastLine[start:end])
-                    logging.exception(str(e))
-                    temperature = "-999"
-            else:
-                temperature = "-999"
-                logging.error("erroneous reading: {0}".format(lastLine[start:end]))
-            logging.debug(
-                "Read temperature " + str(temperature)
-            )  # + ' ' + str(lastLine))
+        end = lastLine.find("\\n", start)
+        if (
+            end == -1
+        ):  # Different thermometers may parse differently. These conditionals may need to expand.
+            end = lastLine.find(" ", start)
+        if end == -1:  # Maybe just go to the end?
+            end = len(lastLine) - 1
 
-            self.nextTempReadingTime = datetime.now() + timedelta(seconds=1)
+        logging.debug(
+            "found "
+            + str(start)
+            + " "
+            + str(end)
+            + " "
+            + lastLine
+            + " "
+            + lastLine[start:end]
+        )
+        # Make sure that we have a start and an end and that there is something between them
+        if start > -1 and end > -1 and end - start > 2:
+            try:
+                self.lastTemp = float(lastLine[start:end])
+                self.nextTempReadingTime = datetime.now() + timedelta(seconds=1)
+            except Exception as e:
+                logging.error("Error converting temperature reading")
+                logging.error(lastLine[start:end])
+                logging.exception(str(e))
+                self.lastTemp = "-999"
+        else:
+            self.lastTemp = "-999"
+            logging.error("erroneous reading: {0}".format(lastLine[start:end]))
+        logging.debug(
+            "Read temperature " + str(self.lastTemp)
+        )  # + ' ' + str(lastLine))
 
         return self.lastTemp
