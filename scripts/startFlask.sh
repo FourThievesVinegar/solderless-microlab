a01f404af041fba895373c5a8296b67469f6a77f
Sparkles <2106250+sparklestheunicorn@users.noreply.github.com>
2024-07-27 11:56:30 -0400

    Don't install requirements.txt every time. Formatting and cleanup.

--- scripts/startFlask.sh
+++ scripts/startFlask.sh
@@ -3,6 +3,7 @@
 cd ./backend
 python3 -m venv env
 source env/bin/activate
-sudo pip3 install -r requirements.txt
+# We only need to install the packages the first time
+# sudo pip3 install -r requirements.txt
 
 sudo python3 main.py production
