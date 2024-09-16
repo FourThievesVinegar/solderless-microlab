b2d55576617ec0572d32873efe6f91c4531dd4f2
Fall Byrd <fall.byrd@protonmail.com>
2024-09-15 18:29:51 -0700

    Moved grblWrite into base class ReagentDispenser, changed PeristalticPump and SyringePump to use it, and removed the duplicated code in each of the two related aforementioned files. Additionally added type hints and minor vairable name changes.

--- backend/hardware/reagentdispenser/base.py
+++ backend/hardware/reagentdispenser/base.py
@@ -33,3 +33,31 @@ class ReagentDispenser(ABC):
                     Maximum speed the pump can dispense in ml/s
         """
         pass
+
+    @classmethod
+    def grblWrite(cls, grblSer, command, retries=3):
+        """
+        Writes the given command to grbl.
+
+        :param grblSer:
+        Serial device to write the command to
+
+        :param command:
+        String of grbl command to execute
+
+        :return:
+        None
+        """
+        grblSer.reset_input_buffer()
+        grblSer.write(bytes(command, "utf-8"))
+        # Grbl will execute commands in serial as soon as the previous is completed.
+        # No need to wait until previous commands are complete. Ok only signifies that it
+        # parsed the command
+        response = grblSer.read_until()
+        if "error" in str(response):
+            if retries > 0:
+                cls.grblWrite(grblSer, command, retries - 1)
+            else:
+                raise Exception(
+                    "grbl error: {0} for command: {1}".format(response, command)
+                )
