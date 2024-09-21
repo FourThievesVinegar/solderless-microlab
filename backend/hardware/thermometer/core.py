187e575d1811bba6e3b696aab5664b9e0ca54646
Fall Byrd <fall.byrd@protonmail.com>
2024-09-20 23:52:47 -0700

    Added exception handling around attempting to load the w1therm sensor

--- backend/hardware/thermometer/core.py
+++ backend/hardware/thermometer/core.py
@@ -3,14 +3,27 @@ This module contains the implementations of the thermometer. See base.py for the
 and either the individual files or backend/config.py for configuration information.
 """
 
-from hardware.thermometer.w1_therm import W1TempSensor
 from hardware.thermometer.serial import SerialTempSensor
 from hardware.thermometer.serial_simulation import SerialTempSensorSimulation
+
+W1_IMPORT_ERROR = False
+
+try:
+    from w1thermsensor import W1ThermSensor
+except Exception:
+    # We have to catch broadly for this exception as there is an error defined
+    # in the w1thermsensor library to attempt to catch this issue
+    # however the exception is triggered on any module import as
+    # the load is attempted in the __init__ file in the base of the repo
+    # so no imports will work at all if there is a load error
+    W1_IMPORT_ERROR = True
+else:
+    from hardware.thermometer.w1_therm import W1TempSensor
 
 
 def createThermometer(thermometerConfig: dict, devices: dict):
     thermometerType = thermometerConfig['implementation']
-    if thermometerType == "w1_therm":
+    if thermometerType == "w1_therm" and not W1_IMPORT_ERROR:
         return W1TempSensor()
     elif thermometerType == "serial":
         return SerialTempSensor(thermometerConfig)
