2ecda7409615b7625d94db2ea73bbf74e8099c88
Fall Byrd <fall.byrd@protonmail.com>
2024-12-01 23:03:01 -0800

    Moved process creation to 'spawn' and added some extra cleanup steps for a clean teardown of the backend

--- backend/main.py
+++ backend/main.py
@@ -4,17 +4,23 @@ flask backend API.
 Starts the flask application on the configured port (default 8081)
 Look in api.routes for the actual api code
 """
-
-from multiprocessing import Process, Queue
-from microlab.core import startMicrolabProcess
-from api.core import run_flask
-import config
-import multiprocessing_logging
-
+import sys
+import time
+import signal
 import logging
 import logging.handlers as handlers
+
+import multiprocessing_logging
+
+import config
+
+from multiprocessing import Process, Queue, set_start_method
+
+from api.core import run_flask
+from microlab.core import startMicrolabProcess
 from util.logFormatter import MultiLineFormatter
-import sys
+
+LOGGER = logging.getLogger(__name__)
 
 
 def setupLogging():
@@ -37,25 +43,100 @@ def setupLogging():
     multiprocessing_logging.install_mp_handler()
 
 
+class BackendManager:
+
+    def __init__(self):
+        self._q1 = Queue()
+        self._q2 = Queue()
+
+        self._processes = []
+        self._queues = []
+
+    def _are_processes_alive(self) -> bool:
+        return any([process.is_alive() for process in self._processes])
+
+    def _cleanup_queues(self):
+        LOGGER.debug('Cleaning up queues')
+
+        self._q1.close()
+        self._q2.close()
+
+        self._q1.join_thread()
+        self._q2.join_thread()
+
+        LOGGER.debug('Completed cleanup up queues')
+
+    def _cleanup_processes(self):
+        LOGGER.debug('Cleaning up processes')
+
+        while self._are_processes_alive():
+            for proc in self._processes:
+                try:
+                    self._q1.get_nowait()
+                except Exception:
+                    pass
+
+                try:
+                    self._q2.get_nowait()
+                except Exception:
+                    pass
+
+                if proc.is_alive():
+                    LOGGER.debug(f'Attempting to join proc: {proc.pid}')
+                    proc.join(timeout=1)
+
+        LOGGER.debug('Completed cleaning up processes')
+
+    def _cleanup_everything(self):
+        self._cleanup_processes()
+        self._cleanup_queues()
+
+    def _handle_exit_signals(self, signum, frame):
+        LOGGER.debug('Beginning to handle exit signals in BackendManager')
+        self._cleanup_everything()
+        LOGGER.debug('Completed handling exit signals in BackendManager')
+
+    def _start_microlab(self):
+        self._microlab_manager_process = Process(
+            target=startMicrolabProcess, args=(self._q1, self._q2), name="microlab"
+        )
+
+        self._processes.append(self._microlab_manager_process)
+
+        LOGGER.debug('Starting the microlab process')
+        self._microlab_manager_process.start()
+        LOGGER.debug(f'microlab process pid: {self._microlab_manager_process.pid}')
+
+    def _start_server(self):
+        self._flaskProcess = Process(target=run_flask, args=(self._q2, self._q1), name="flask", daemon=True)
+        self._processes.append(self._flaskProcess)
+        LOGGER.debug('Starting the server process')
+        self._flaskProcess.start()
+        print(f'server process pid: {self._flaskProcess.pid}')
+
+    def run(self):
+        config.initialSetup()
+
+        LOGGER.info("### STARTING MAIN MICROLAB SERVICE ###")
+
+        self._start_microlab()
+        self._start_server()
+
+        signal.signal(signal.SIGINT, self._handle_exit_signals)
+        signal.signal(signal.SIGTERM, self._handle_exit_signals)
+
+        while self._are_processes_alive():
+            time.sleep(0.1)
+
+        LOGGER.debug("### ENDING MICROLAB SERVICE EXECUTION ###")
+
+
+def main():
+    backend_manager = BackendManager()
+    backend_manager.run()
+
+
 if __name__ == "__main__":
-    config.initialSetup()
+    set_start_method('spawn')
     setupLogging()
-
-    logging.info("### STARTING MAIN MICROLAB SERVICE ###")
-
-    q1 = Queue()
-    q2 = Queue()
-
-    microlabProcess = Process(
-        target=startMicrolabProcess, args=(q1, q2), name="microlab"
-    )
-    microlabProcess.start()
-    flaskProcess = Process(target=run_flask, args=(q2, q1), name="flask")
-    flaskProcess.start()
-
-    microlabProcess.join()
-    flaskProcess.join()
-    q1.close()
-    q2.close()
-    q1.join_thread()
-    q2.join_thread()
+    main()
