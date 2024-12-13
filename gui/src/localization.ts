e4b69bf456ae621fc8c3a7f25d24de4e2d6c2b72
spkry3 <spkry@tuta.io>
2024-12-13 20:24:01 +0200

    fixed ru not being found

--- gui/src/localization.ts
+++ gui/src/localization.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import en from './locales/en/strings.json'
 import es from './locales/es/strings.json'
+import ru from './locales/ru/strings.json'
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
