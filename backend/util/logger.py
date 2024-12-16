90f91c6559a615c0e20c278642c793c884edac53
Fall Byrd <fall.byrd@protonmail.com>
2024-12-15 19:33:59 -0800

    Added a new multiprocess logger

---
+++ backend/util/logger.py
@@ -0,0 +1,107 @@
+import logging
+import logging.handlers
+import queue
+import sys
+import traceback
+
+import config
+
+from typing import Union
+from multiprocessing import Queue
+from util.logFormatter import MultiLineFormatter
+
+
+class MultiprocessingLogger:
+
+    _logging_queue = None
+    _configured_loggers = {}
+
+    _processing_logger = None
+
+    @classmethod
+    def initialize_logger(cls, logging_queue: Union[Queue, None] = None) -> None:
+        """ Initialize the logger.
+
+        Must be called in the base process that start the other processes without any argument.
+        In other process it must be called with a dedicated logging queue."""
+        if logging_queue is None:
+            logging_queue = Queue()
+
+        cls._logging_queue = logging_queue
+
+    @classmethod
+    def get_logging_queue(cls) -> Union[Queue, None]:
+        return cls._logging_queue
+
+    @classmethod
+    def get_logger(cls, logger_name: str) -> logging.Logger:
+        # We only need to configure once, multiple calls to logging.getLogger(<logger_name>)
+        # will return the same configured instance
+        if logger_name in cls._configured_loggers:
+            return logging.getLogger(logger_name)
+
+        logger = logging.getLogger(logger_name)
+        logger.addHandler(logging.handlers.QueueHandler(cls._logging_queue))
+        logger.setLevel(config.microlabConfig.logLevel)
+
+        cls._configured_loggers[logger_name] = True
+
+        return logger
+
+    @classmethod
+    def cleanup_logging(cls):
+        # Without the retries check added here I would occasionally see a hang here
+        # on cleanup
+        retries = 0
+        while not cls._logging_queue.empty() and retries < 5:
+            cls.process_logs()
+            retries += 1
+
+    @classmethod
+    def _get_processing_logger(cls, logger_name: str) -> logging.Logger:
+        if logger_name in cls._configured_loggers:
+            return logging.getLogger(logger_name)
+
+        formatter = MultiLineFormatter(fmt="%(asctime)s %(name)-10s [%(levelname)s]: %(message)s")
+
+        log_handlers = []
+
+        rotating_file_handler = logging.handlers.RotatingFileHandler(
+                "{0}/microlab.log".format(config.microlabConfig.logDirectory),
+                maxBytes=config.microlabConfig.logFileMaxBytes,
+                backupCount=config.microlabConfig.logFileBackupCount,
+            )
+        rotating_file_handler.setFormatter(formatter)
+        log_handlers.append(rotating_file_handler)
+
+        if config.microlabConfig.logToStderr:
+            stderr_logger = logging.StreamHandler(sys.stderr)
+            stderr_logger.setFormatter(formatter)
+            log_handlers.append(stderr_logger)
+
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(config.microlabConfig.logLevel)
+        for handler in log_handlers:
+            logger.addHandler(handler)
+
+        cls._configured_loggers[logger_name] = True
+
+        return logger
+
+    @classmethod
+    def process_logs(cls):
+        # print('processing logs')
+        try:
+            record = cls._logging_queue.get_nowait()
+            logger = cls._get_processing_logger(record.name)
+            logger.handle(record)
+        except queue.Empty:
+            return
+        except ValueError as e:
+            print(f'Value error: {e}')
+            return
+        except Exception as e:
+            sys.stderr.write(f'Encountered exception: {e} while attempting to process logs. Traceback:\n')
+            sys.stderr.write(traceback.format_exc())
+
+
